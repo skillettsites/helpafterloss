4b4517ddc3bb459c57e40e9090c33245c3569817
skillettsites <davidskillett@hotmail.co.uk>
2026-05-20 18:09:30 +0100

    redirects: 301 three removed URLs to live equivalents (clears GSC 404s)

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,13 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      { source: "/how-we-work", destination: "/about", permanent: true },
+      { source: "/before-you-go", destination: "/checklist", permanent: true },
+      { source: "/blog/death-without-will-uk", destination: "/blog/inheritance-tax-guide-after-death", permanent: true },
+    ];
+  },
 };
 
 export default nextConfig;
